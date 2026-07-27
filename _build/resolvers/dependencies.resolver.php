5e0a91e4adc902f841298642ed6927126e263f50
Mark Hamstra <hello@markhamstra.com>
2026-07-27 16:39:44 +0200

    Fix guzzle conflict by updating dependencies and increasing minimum PHP version to 8.2

--- _build/resolvers/dependencies.resolver.php
+++ _build/resolvers/dependencies.resolver.php
@@ -17,7 +17,7 @@ switch ($options[xPDOTransport::PACKAGE_ACTION]) {
          * Define required packages: name => minimum version
          */
         $packages = [
-            'Guzzle7' => '1.0.0',
+            'Guzzle7' => '7.8.0',
         ];
 
         /** @var modTransportProvider|\MODX\Revolution\Transport\modTransportProvider $provider */
